5f49eddf102b1865ade8563d8664ed5c421d5d67
ssse0y <ko527ko@gmail.com>
2022-11-02 10:28:16 +0900

    #3

--- main.c
+++ main.c
@@ -3,22 +3,24 @@
 #define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+int main(int argc, char *argv[]) {
    
-   int i, avg;
-   int sum;
+   int i;
    int grade[SIZE];
+   int score[SIZE];
    
-   sum=0;
    for(i=0; i<SIZE; i++){
-      printf("학생 성적을 입력하세요.:");
-      scanf("%d", &grade[i]);
-      sum += grade[i]; 
-	  }
+      grade[i]=rand()%100;
+   }
    
+     for(i=0; i<SIZE; i++){
+      score[i]=grade[i];
+   }
    
-   avg=sum/SIZE;
-   printf("성적 평균 : %d\n", avg);
+   for(i=0; i<SIZE; i++){
+      printf("score[%d]=%d\n",i,score[i]);
+   }
+
    return 0;
 }
-
+   
